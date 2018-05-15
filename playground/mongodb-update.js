625492edf18e41a0c2966688b24955b789f66a6f
mohamedelbalshy <mohamedbalshy2014@gmail.com>
2018-05-15 04:47:59 +0200

    Add update Script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,23 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
+    if(err)
+        return console.log('Unable to connect to MongoDB', err)
+    
+    console.log('Connected to MongoDB!');
+    db.collection('Todos').findOneAndUpdate({
+        _id: new ObjectID('5afa1acc2a5b2d0b70f96120')
+    },{
+        $set:{
+            completed: true
+        }
+    },{
+        returnOriginal: false
+    }).then((result)=>{
+        console.log(result)
+    })
+    
+
+
+    //db.close();
+})
